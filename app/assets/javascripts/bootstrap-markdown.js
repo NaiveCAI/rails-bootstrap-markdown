bc41263eb3dbed663cf47c1256d7463a76c5800e
tracy <tracy@ekohe.com>
2016-10-11 11:05:48 +0800

    Make one enter to <br> global!!

--- app/assets/javascripts/bootstrap-markdown.js
+++ app/assets/javascripts/bootstrap-markdown.js
@@ -446,7 +446,7 @@
         var val = this.$textarea.val();
         if(typeof markdown == 'object') {
           content = markdown.toHTML(val);
-          content = unescape(escape(content).replace(/%0A/, "%3Cbr%3E"));
+          content = unescape(escape(content).replace(/%0A/g, "%3Cbr%3E"));
         }else if(typeof marked == 'function') {
           content = marked(val);
         } else {
